5a52aa5b791c52aac3fcc69695bc7be2b9882e97
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-19 17:53:01 -0500

    Put method to get a file's content in File class
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- http-server/src/httpserver/io/File.py
+++ http-server/src/httpserver/io/File.py
@@ -15,3 +15,13 @@ class File:
 
     def get_html(self):
         return f"<li>{os.path.basename(self.path)}</li>"
+
+    def get_contents(self):
+        try:
+            return open(self.path).read(), 200, "OK"
+        except FileNotFoundError:
+            return "File not found.", 404, "Not Found"
+        except IsADirectoryError:
+            return "You cannot read a directory's contents. Well, technically you can. But I won't allow it.", 400, "Bad Request"
+        except NotADirectoryError:
+            return "Where do you think you're going?", 400, "Bad Request"
