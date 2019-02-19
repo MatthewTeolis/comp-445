2d217554ae66097a699abb35aacae7943c538eba
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-19 11:04:53 -0500

    Error handling
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- http-server/src/httpserver/io/Directory.py
+++ http-server/src/httpserver/io/Directory.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 class Directory:
 
-    def __init__(self, path: str, children: list):
+    def __init__(self, path: str, children: iter):
         self.path = path
         self.children = children
 
@@ -12,13 +12,13 @@ class Directory:
         return OrderedDict(
             file=os.path.basename(self.path),
             isDir=True,
-            children=self.children
+            children=[child.__repr__() for child in self.children]
         )
 
     def get_html(self):
         return f"<li>" \
             f"{os.path.basename(self.path)}" \
             f"<ul>" \
-            f"{''.join([child for child in self.children])}" \
+            f"{''.join([child.get_html() for child in self.children])}" \
             f"</ul>" \
             f"</li>"
