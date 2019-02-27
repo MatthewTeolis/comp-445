80d9fde6698664bf3921359903d9dd2e8a85ba0d
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-27 16:33:24 -0500

    Add POST functionality
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- http-server/src/httpserver/io/File.py
+++ http-server/src/httpserver/io/File.py
@@ -29,3 +29,12 @@ class File:
             return "You cannot read a directory's contents. Well, technically you can. But I won't allow it.", 400, "Bad Request"
         except NotADirectoryError:
             return "Where do you think you're going?", 400, "Bad Request"
+
+    def write(self, content):
+        try:
+            message = "overwrote" if os.path.exists(self.path) else "created and wrote to"
+            with open(self.path, 'w') as file:
+                file.write(content)
+                return f"Successfully {message} {os.path.basename(self.path)}. You can find it at {self.path}", 200, "OK"
+        except IsADirectoryError:
+            return "You cannot overwrite a directory's content.", 400, "Bad Request"
