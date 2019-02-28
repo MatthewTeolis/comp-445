2301dc08aaab412928c65df2eadd87cbdfaeb1c2
Matthew Teolis <matthewteolis@gmail.com>
2019-02-27 23:52:48 -0500

    Implemented multi-threading for multiple clients
    Signed-off-by: Matthew Teolis <matthewteolis@gmail.com>

--- http-server/src/httpserver/http/HttpServer.py
+++ http-server/src/httpserver/http/HttpServer.py
@@ -7,18 +7,6 @@ class HttpServer:
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('0.0.0.0', port))
-        self.clients = list()
-
-    def __recv_all(self):
-        total_data = []
-
-        while True:
-            data = self.socket.recv(1024)
-            if not data:
-                break
-            total_data.append(data.decode('utf-8'))
-
-        return ''.join(total_data)
 
     def listen(self):
         self.socket.listen(5)
